a4e13eee713ec9eefbf5920a9e15b0b11e452cea
ajarms <ajarms11@gmail.com>
2025-04-01 16:25:58 -0500

    Mob spawning logic

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -8,6 +8,11 @@ public partial class Main : Node
 
     private int _score;
 
+    public override void _Ready()
+    {
+        NewGame();
+    }
+
     public void GameOver(){
         this.GetNode<Timer>("MobTimer").Stop();
         this.GetNode<Timer>("ScoreTimer").Stop();
@@ -29,6 +34,29 @@ public partial class Main : Node
         spawn mob at random location along the mob spawn path
         set a random speed for the mob between 150 and 250
         */
+
+        Mob mobInstance = MobScene.Instantiate<Mob>();
+
+        var mobSpawnLocation = this.GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        // the path is clockwise around the perimeter of the screen,
+        // so turn clockwise perpendicular to the path to face the interior of the screen
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        mobInstance.Position = mobSpawnLocation.Position;
+
+        // TODO this is taken from the tutorial, but it has an edge case:
+        // if the mob is spawned close to a corner it's perpendicular heading will be closely
+        // parallel to the screen edge, and if this randomly tilts it in the wrong direction
+        // it will immediately travel off of the screen after being spawned.
+        // To avoid this, it could either detect corners, or only allow for a negative tilt
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+
+        var velocity = new Vector2((float)GD.RandRange(150, 250), 0).Rotated(direction);
+        mobInstance.LinearVelocity = velocity;
+
+        this.AddChild(mobInstance);
     }
 
     public void OnStartTimerTimeout(){
