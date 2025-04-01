5adabbec85052331c63d681b990998ccb8571fcc
ajarms <ajarms11@gmail.com>
2025-04-01 16:26:09 -0500

    HUD scene

---
+++ scripts/HUD.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class HUD : CanvasLayer
+{
+    [Signal]
+    public delegate void StartGameEventHandler();
+
+    public void ShowMessage(string messageText){
+        var message = this.GetNode<Label>("Message");
+        message.Text = messageText;
+        message.Show();
+
+        this.GetNode<Timer>("MessageTimer").Start();
+    }
+
+    public async Task ShowGameOver(){
+        ShowMessage("Game Over");
+
+        var messageTimer = this.GetNode<Timer>("MessageTimer");
+        await ToSignal(messageTimer, Timer.SignalName.Timeout);
+
+        var message = this.GetNode<Label>("Message");
+        message.Text = "Dodge the Creeps!";
+        message.Show();
+
+        await ToSignal(GetTree().CreateTimer(1.0f), SceneTreeTimer.SignalName.Timeout);
+        this.GetNode<Button>("StartButton").Show();
+    }
+
+    public void UpdateScoreLabel(int score){
+        this.GetNode<Label>("ScoreLabel").Text = score.ToString();
+    }
+}
